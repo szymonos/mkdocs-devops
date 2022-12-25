62d89cd2f8e73ebae3b630d486d8555ac5807181
Szymon Osiecki <szymonos@outlook.com>
2022-12-25 09:42:54 +0100

    fix(sh): linters
    change megalinter rev to v6

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -1,7 +1,7 @@
 # *Megalinter
 # https://megalinter.io/latest/mega-linter-runner/
 # run mega-linter-runner without installation by using npx
-npx mega-linter-runner -r beta --flavor documentation -e 'ENABLE=MARKDOWN' -e 'SHOW_ELAPSED_TIME=true'
+npx mega-linter-runner -r v6 --flavor documentation -e 'ENABLE=MARKDOWN' -e 'SHOW_ELAPSED_TIME=true'
 
 # *Cspell
 # https://cspell.org/docs/installation/
