12c89a8683741aa8a6545af2dd3749c4a4d905f7
Szymon Osiecki <szymonos@outlook.com>
2023-04-02 18:33:29 +0200

    refactor(sh): linters.sh

--- scripts/linters.sh
+++ scripts/linters.sh
@@ -1,10 +1,20 @@
 # *Megalinter
 # https://megalinter.io/latest/mega-linter-runner/
 # run mega-linter-runner without installation by using npx
-npx mega-linter-runner -r v6 --flavor documentation -e 'ENABLE=MARKDOWN'
+npx mega-linter-runner -r v6 --flavor documentation -e 'ENABLE=MARKDOWN' -e 'SHOW_ELAPSED_TIME=true' -p ./docs
 
 # *Cspell
 # https://cspell.org/docs/installation/
 sudo npm install -g cspell@latest
 # run cspell
 cspell "**"
+
+# *Conda environment
+# create/update mkdocs environment
+./conda.ps1 setup
+# activate/deactivate mkdocs environment
+./conda.ps1 activate
+./conda.ps1 deactivate
+
+# *Run mkdocs development server
+mkdocs serve
